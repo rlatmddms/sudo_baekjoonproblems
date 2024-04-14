dcac308553aceaba3b65ab2570dc677d3852705a
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2024-04-14 20:45:40 +0900

    Update bubble_sorting1.c

--- sorting/bubble_sorting/bubble_sorting1.c
+++ sorting/bubble_sorting/bubble_sorting1.c
@@ -1,5 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
+#define arr_size 10
+//arr_size 라는 글자 자체를 10과 동일시 하게 만듦
+
 int main() {
-	
-	return 0;
+    int arr[arr_size] = {1,6,2,4,8,2,45,77,12,3};
+    for(int i = 0; i < arr_size; i++) {
+        for(int j = i+1; j < arr_size; j++) {
+            if(arr[i] > arr[j]) {
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+    }
+    for(int i = 0; i < arr_size; i++) 
+        printf("%d, ",arr[i]);
+    return 0;
 }
